61653c68addd970da76e2616d17df957cb45f024
Sergey <167028015+sirgay1@users.noreply.github.com>
2025-03-12 02:12:57 +0300

    Add files via upload

---
+++ structs.hpp
@@ -0,0 +1,18 @@
+#ifndef STRUCTS_HPP
+#define STRUCTS_HPP
+#include <iostream>
+
+struct Coefficients {
+    double a;
+    double b;
+    double c;
+};
+
+struct Roots {
+    double im1;
+    double re1;
+    double im2;
+    double re2;
+};
+
+#endif
