d9ca9dc56d1ad56328df9570dbc9464fcf282308
Sergey <167028015+sirgay1@users.noreply.github.com>
2025-03-12 02:13:26 +0300

    Add files via upload

---
+++ input_n_output.hpp
@@ -0,0 +1,11 @@
+#ifndef INPUT_N_OUTPUT_HPP
+#define INPUT_N_OTPUT_HPP
+#include <iostream>
+#include "structs.hpp"
+
+using namespace std;
+
+void input(Coefficients* coefficients);
+void output(Roots* roots, double D);
+
+#endif
