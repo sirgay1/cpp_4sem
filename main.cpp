8f290c08bf14f05a0783b16a5ad2e9f61a9a26aa
Sergey <167028015+sirgay1@users.noreply.github.com>
2025-03-31 21:53:52 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+﻿#include "teacher.hpp"
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	Teacher teacher;
+
+	teacher.input_n_reading("tasks.txt");
+	teacher.open_n_read("students.txt");
+	teacher.create_queue();
+	teacher.make_answers();
+	teacher.check_works();
+	teacher.publish_results();
+
+	return 0;
+}
